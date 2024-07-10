a1e8d53a5b3c00f6dc276bc0cad8fef735dfe257
Shakil Anwar <38348848+shakilanwar1998@users.noreply.github.com>
2024-07-10 19:50:26 +0600

    Component breakdown

---
+++ resources/js/Components/Question/QuestionBlock.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="question-block">
+        <div class="question">
+            <h3 class="question-title">{{ question.question }}</h3>
+
+            <QuestionImage v-if="question.image_url" :url=question.image_url />
+
+            <ul class="list-group mt-5">
+                <li class="flex items-center mb-5">
+                    <input type="radio" id="option-a" name="question" value="A" class="mr-2">
+                    <label for="option-a" class="mr-4">A</label> {{ question.option_a }}
+                </li>
+                <li class="flex items-center mb-5">
+                    <input type="radio" id="option-b" name="question" value="B" class="mr-2">
+                    <label for="option-b" class="mr-4">B</label> {{ question.option_b }}
+                </li>
+                <li class="flex items-center mb-5">
+                    <input type="radio" id="option-c" name="question" value="C" class="mr-2">
+                    <label for="option-c" class="mr-4">C</label> {{ question.option_c }}
+                </li>
+                <li class="flex items-center mb-5">
+                    <input type="radio" id="option-d" name="question" value="D" class="mr-2">
+                    <label for="option-d" class="mr-4">D</label> {{ question.option_d }}
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+import QuestionImage from "@/Components/Question/QuestionImage.vue";
+
+defineProps({
+    question: {
+        type: Object,
+    }
+});
+
+</script>
---
+++ resources/js/Components/Question/QuestionImage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="question-image">
+        <img :src=url alt="">
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    url: {
+        type: String,
+    }
+});
+</script>
