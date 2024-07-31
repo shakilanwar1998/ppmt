557dd709f3e0e4e522f991d7ef80c528cfff192a
Shakil Anwar <38348848+shakilanwar1998@users.noreply.github.com>
2024-08-01 00:30:26 +0600

    Homepage Initiate (Partial)

---
+++ resources/js/Components/Home/Exam.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="stamp-card-inner">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="stamp-img-block">
+                    <img src="assets/img/stamp.jpg" alt="">
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="stamp-detail d-flex justify-content-between align-items-center h-100">
+                    <div class="stamp-left">
+                        <h2 class="stamp-heading">qr digital stamp card sale</h2>
+                        <p class="stamp-point"><span>Point: 60</span></p>
+                        <div class="stamp-date-time">
+                            <div class="row">
+                                <div class="col-6">
+                                    <i class="fa-regular fa-clock"></i> <span class="time">15:18 pm</span>
+                                </div>
+                                <div class="col-6">
+                                    <i class="fa-regular fa-calendar"></i><span class="date">31/05/2024</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="stamp-right">
+                        <button type="button" class="stamp-btn">Test</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <Head title="PPMT - Pythagoras Progressive Math Test" />
+
+    <Exam />
+</template>
+<script setup lang="ts">
+import Exam from "@/Components/Home/Exam.vue";
+import {Head} from "@inertiajs/vue3";
+</script>
